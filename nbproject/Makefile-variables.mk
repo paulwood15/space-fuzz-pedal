9262fb73f6bf67b100717d0ddc62130671c4a241
Paul Wood <904paul@gmail.com>
2018-07-12 10:30:34 -0400

    Initial project setup

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=space-fuzz-pedal.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/space-fuzz-pedal.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=space-fuzz-pedal.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/space-fuzz-pedal.tar
